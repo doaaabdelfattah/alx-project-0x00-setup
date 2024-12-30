12063bf9af98b1f7ccf9776ffe287432951bec41
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-12-30 22:33:49 +0200

    upload tasks

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,12 @@
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <Button size="medium" shape="rounded-md" />
     </div>
   );
 };
